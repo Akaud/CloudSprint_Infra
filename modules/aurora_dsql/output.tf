90a9212192115bf8651cd3f46a8bc67d74fc864a
Khusan-Khujakeldiev <KhusanKhujakeldiev@gmail.com>
2025-08-28 15:25:04 +0300

    fix: Change Aurora to provisioned mode for eu-west-1 compatibility

--- modules/aurora_dsql/output.tf
+++ modules/aurora_dsql/output.tf
@@ -3,12 +3,27 @@ output "db_cluster_endpoint" {
   value       = aws_rds_cluster.postgres_cluster.endpoint
 }
 
+output "db_cluster_reader_endpoint" {
+  description = "The reader endpoint of the PostgreSQL cluster."
+  value       = aws_rds_cluster.postgres_cluster.reader_endpoint
+}
+
+output "db_instance_endpoint" {
+  description = "The endpoint of the PostgreSQL instance."
+  value       = aws_rds_cluster_instance.postgres_instance.endpoint
+}
+
 output "db_proxy_endpoint" {
-  description = "The endpoint of the RDS Proxy for connection pooling."
+  description = "The RDS Proxy endpoint for connection pooling."
   value       = aws_db_proxy.db_proxy.endpoint
 }
 
 output "db_name" {
   description = "The name of the provisioned database."
   value       = aws_rds_cluster.postgres_cluster.database_name
+}
+
+output "cluster_identifier" {
+  description = "The cluster identifier."
+  value       = aws_rds_cluster.postgres_cluster.cluster_identifier
 }
